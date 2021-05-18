37d1b3c45b03b50e20e7e798eddf593adf7b41cd
Joe Dimartino <j.dimartino11@gmail.com>
2021-05-18 17:57:13 -0400

    Expand integration tests

--- src/integration_test/__tests__/integrationTest.spec.js
+++ src/integration_test/__tests__/integrationTest.spec.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const javaApiUrl = `http://localhost:${PORT}`;
 const axios = jest.requireActual("axios");
-const coffeeApiResponse = require("../../__stubs__/getFavoriteCoffeeResponse.json");
+const getRandomCoffeeResponse = require("../../__stubs__/getFavoriteCoffeeResponse.json");
+const postFavoriteCoffeeResponse = require("../../__stubs__/postFavoriteCoffeeResponse.json");
+const getFavortieCoffeeResponse = getRandomCoffeeResponse;
+const coffeToPost = getRandomCoffeeResponse;
 
 describe("javaApi", () => {
   describe("/javaApi", () => {
@@ -10,7 +13,39 @@ describe("javaApi", () => {
       describe("200 Response", () => {
         it("should respond with coffee data", async () => {
           const response = await axios.get(`${javaApiUrl}/javaApi`);
-          expect(response.data).toMatchObject(coffeeApiResponse);
+          expect(response.data).toMatchObject(getRandomCoffeeResponse);
+        });
+      });
+    });
+  });
+  describe("/javaApifavorites", () => {
+    describe("POST request", () => {
+      describe("when coffee info is provided", () => {
+        it("should respond with an object containg the username and the coffee data", async () => {
+          const response = await axios.post(
+            `${javaApiUrl}/javaApi/favorite/joed`,
+            coffeToPost
+          );
+          expect(response.data).toMatchObject(postFavoriteCoffeeResponse);
+        });
+      });
+    });
+    describe("GET request", () => {
+      describe("when the username is in the database", () => {
+        describe("when coffee info exists in the database", () => {
+          it("should respond the coffee data for that user", async () => {
+            const response = await axios.get(
+              `${javaApiUrl}/javaApi/favorite/joed`
+            );
+            expect(response.data).toMatchObject(getFavortieCoffeeResponse);
+          });
+        });
+      });
+      describe("when the username is NOT in the database", () => {
+        it("should respond with a 404 error", async () => {
+          await expect(
+            axios.get(`${javaApiUrl}/javaApi/favorite/karen`)
+          ).rejects.toThrow(new Error("Request failed with status code 404"));
         });
       });
     });
